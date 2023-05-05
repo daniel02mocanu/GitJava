d186e81d053e820768cd8c25e88b504e9ed9be3c
Diurno <Diurno@pc0109.mshome.net>
2023-05-05 13:05:40 +0200

    PRIMERA CONFIRMACION

---
+++ Proyecto1/src/gitr/Principalñ.java
@@ -0,0 +1,10 @@
+package gitr;
+
+public class Principalñ {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Control de versiones con Git");
+	}
+
+}
